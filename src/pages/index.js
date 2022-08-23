37567f788206b54967269ee1f0d7ab51dcbb4400
marveldev <marvellousalika48@gmail.com>
2022-08-23 14:47:10 +0100

    Add our MDX file - install gray-matter - return posts from MDX file using getStaticProps functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import styles from '../../styles/home.module.css'
+import fs from 'fs'
+import * as path from 'path'
+import matter from 'gray-matter'
 
-const Home = () => {
+const Home = ({ posts }) => {
+  console.log(posts)
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
@@ -12,3 +16,22 @@ const Home = () => {
 }
 
 export default Home
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync(path.join('src', 'posts'))
+  const posts = files.map((filename) => {
+    const markdownWithMeta = fs.readFileSync(path.join('src', 'posts', filename))
+    const { data } = matter(markdownWithMeta)
+
+    return {
+      data,
+      slug: filename.split('.')[0],
+    }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
